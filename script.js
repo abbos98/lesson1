845f00b500bbb9d09db1e73eee120ea1a5cb5b10
Abbos <abbos.pankiy2018@gmail.com>
2024-02-24 21:44:33 +0900

    abbos

---
+++ script.js
@@ -0,0 +1,19 @@
+// juft va toq sonlar //
+let num = 11
+num % 2 === 0 && console.log('juft son');
+num % 2 !== 0 && console.log('toq son')
+
+// Ball sistemasi //
+let ball = 59;
+ball >= 86 && ball <= 100 && console.log('A');
+ball >= 61 && ball <= 85 && console.log('B');
+ball >= 50 && ball <= 60 && console.log('C');
+ball >= 50 && console.log('F');
+
+// Meva narxlarini topish //
+let number = 'tarvuz';
+
+number === 'olma' && console.log(10000);
+number === 'nok' && console.log(12000);
+number === 'tarvuz' && console.log(15000);
+number !== 'olma'&& number !== 'nok'&& number !== 'tarvuz'&& console.log('bunday meva yoq'); 
